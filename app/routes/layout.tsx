6f93798eb015edeb525cf3ad9c3896d045e61164
itmanbanana <itmanbanana@gmail.com>
2026-06-24 20:30:47 -0400

    Tweak styles of navbar

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,12 +1,14 @@
 import { NavLink, Outlet } from "react-router";
 
+const navStyle = ({ isActive }: { isActive: boolean; }) => isActive ? "text-blue-600" : "text-white";
+
 const PostsLayout = () => {
   return (
     <div className="">
       <nav className="flex justify-center gap-8 mb-8">
-        <NavLink to={``}>Home</NavLink>
-        <NavLink to={`posts`}>All Posts</NavLink>
-        <NavLink to={`posts/new`}>Create New Post</NavLink>
+        <NavLink className={navStyle} to={``}>Home</NavLink>
+        <NavLink className={navStyle} to={`posts`} end={true}>All Posts</NavLink>
+        <NavLink className={navStyle} to={`posts/new`} end={true}>Create New Post</NavLink>
       </nav>
       <Outlet />
     </div>
